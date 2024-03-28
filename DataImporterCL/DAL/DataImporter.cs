08cf8516b48a5381488da2515d056db7d41a93bf
Tafadzwa.Stima <tafadzwa.stima@outlook.com>
2024-03-28 11:19:24 +0200

    implemented convert to csv to xlsx funtion

---
+++ DataImporterCL/DAL/DataImporter.cs
@@ -0,0 +1,43 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.IO;
+
+namespace DataImporterCL.DAL
+{
+    public static class DataImporter
+    {
+        public static void ConvertCsvToExcel()
+        {
+
+            Console.WriteLine("Starting operation ");
+            string csvFilePath = @"C:\src\Decofurn\DecofurnDataImporter\datasample.csv";
+            string excelFilePath = @"C:\src\Decofurn\DecofurnDataImporter\data.xlsx";
+            
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Data");
+
+                using (var reader = new StreamReader(csvFilePath))
+                {
+                    int row = 1;
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                        string[] values = line.Split(',');
+
+                        for (int i = 0; i < values.Length; i++)
+                        {
+                            worksheet.Cell(row, i + 1).Value = values[i];
+                        }
+
+                        row++;
+                    }
+                }
+
+                workbook.SaveAs(excelFilePath);
+            }
+            Console.WriteLine("completed Operation ");
+        }
+    }
+}
+
--- ImporterUI/Program.cs
+++ ImporterUI/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace ImporterUI
+﻿using DataImporterCL.DAL;
+using System;
+
+namespace ImporterUI
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+			try
+			{
+				DataImporter.ConvertCsvToExcel();
+			}
+			catch (System.Exception ex)
+			{
+
+				Console.WriteLine(ex.Message);
+			}
+
         }
     }
 }
